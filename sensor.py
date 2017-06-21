9eec43698f401c1f98c80afc58b00d8bb560773d
leonardo <leonardo@infoprice.co>
2017-06-21 11:17:02 -0300

    audio e sensor

---
+++ sensor.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pin Definitons:
+sensorPin = 23 # Broadcom pin 23 (P1 pin 16)
+
+# Pin Setup:
+GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setup(sensorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Button pin set as input w/ pull-up
+
+# Initial state for LEDs:
+
+print("Here we go! Press CTRL+C to exit")
+try:
+    while 1:
+        if GPIO.input(sensorPin): # button is released
+            print("Detectou sensor")
+        else: # button is pressed:
+            print("Saiu sensor")
+		sleep(0.1)
+
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup() # cleanup all GPIO
